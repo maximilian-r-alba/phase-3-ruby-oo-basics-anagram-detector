6bf9ed23f83d35262c9aab2f3e45eb65bb097dd0
Maximilian Alba <albam476@gmail.com>
2022-12-06 21:23:46 -0500

    commit tree 53315a83e92ad7e6059bd38b13cd70d4839aaef8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match anagram_arr
+        word_arr = @word.chars.sort 
+        #for each word in anagram_arr, create a chars array
+        anagram_chars = anagram_arr.map do |word| 
+            word.chars.sort
+        end
+        #compare each chars array to word_arr
+        matches = anagram_arr.filter do |word|
+       
+            word_arr == word.chars.sort
+           
+        end
+        
+  
+        matches
+    
+    end
+
+
+end
+binding.pry
+0
